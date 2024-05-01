e93d9976f05d5b083926447bbd01932d19ba02af
Corey Stup <CoreyStup@gmail.com>
2024-05-01 11:22:40 -0400

    Added Enbrighten Switch 58433

--- Consolidated Drivers/zwave-switch/src/ge-switch/init.lua
+++ Consolidated Drivers/zwave-switch/src/ge-switch/init.lua
@@ -87,6 +87,7 @@ local GE_SWITCH_FINGERPRINTS = {
   {mfr = 0x0063, prod = 0x4952, model = 0x3235},
   {mfr = 0x0063, prod = 0x4952, model = 0x3237},
   {mfr = 0x0063, prod = 0x4952, model = 0x3238},
+  {mfr = 0x0063, prod = 0x4952, model = 0x3239},  -- Enbrighten Switch 58433
   {mfr = 0x0063, prod = 0x4952, model = 0x3333},  -- UltraPro 59347
   {mfr = 0x0063, prod = 0x4F44, model = 0x3031},  -- GE Direct-Wire Outdoor Switch 12726
   {mfr = 0x0063, prod = 0x4F44, model = 0x3032},
